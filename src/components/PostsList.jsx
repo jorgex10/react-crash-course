b8a77b34592af99dc54470548e9044957633c8a6
Jorge Calvo <jorgecalvo@Jorges-MacBook-Pro.local>
2023-10-23 13:48:27 -0500

    Adding NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Jorge" body="ReactJS is awesome!" />
-      <Post author="Manuel" body="Check out the full course!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Jorge" body="ReactJS is awesome!" />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
   );
 }
 
